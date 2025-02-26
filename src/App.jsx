81ae21a23966dc09b575d253beefb8bf6da80c61
Szadolowski <rafall.curzydlo@gmail.com>
2025-02-26 16:20:40 +0100

    0.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import "./App.css";
+import MainSerch from "./components/MainSerch";
 
 function App() {
   return (
-    <div className=" flex justify-center items-center pt-5">
-      <div className="bg-gray-50 rounded-2xl shadow-2xl border-black w-3xl h-64 p-12 grid grid-cols-4 grid-rows-2 gap-x-4 gap-y-8">
-        <input
-          type="text"
-          value={"test"}
-          className="shadow-2xl col-span-2 bg-gray-500 rounded-[6px] p-1.5"
-        />
-        <input
-          type="text"
-          value={"test"}
-          className="shadow-2xl col-span-2 rounded-[6px] bg-gray-500 p-2.5"
-        />
-        <input type="text" value={"test"} className="shadow-2xl bg-gray-500 rounded-[6px]" />
-        <input type="text" value={"test"} className="shadow-2xl bg-gray-500 rounded-[6px]" />
-        <input type="text" value={"test"} className="shadow-2xl bg-gray-500 rounded-[6px]" />
-        <input type="text" value={"test"} className="shadow-2xl bg-gray-500 rounded-[6px]" />
+    <>
+      <div className="w-full flex flex-row shadow-2xl h-12">
+        <div> Burger </div>
+        <div> Logo </div>
+        <div> Login </div>
       </div>
-    </div>
+      <div className=" flex justify-center items-center pt-5">
+        <MainSerch />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/MainSerch.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+let InputSerch = ({ size = "1", insideText }) => {
+  return (
+    <input
+      type="text"
+      value={insideText}
+      className={`col-span-${size} bg-gray-300 rounded-[6px] p-1.5 w-full h-full`}
+    />
+  );
+};
+
+InputSerch.propTypes = {
+  size: PropTypes.string,
+  insideText: PropTypes.string.isRequired,
+  inputType: PropTypes.string,
+};
+
+function MainSerch() {
+  return (
+    <div className="bg-gray-50 rounded-2xl shadow-2xl border-black p-12 grid grid-cols-4 grid-rows-3 gap-x-4 gap-y-8 place-items-center">
+      <InputSerch insideText={"Marka Samochodu"} size={2} />
+      <InputSerch insideText={"Model pojazdu"} size={2} />
+      <InputSerch insideText={"cena do"} />
+      <InputSerch insideText={"Rok produkcji od"} />
+      <InputSerch insideText={"Typ nadwozia"} />
+      <InputSerch insideText={"rodzaj paliwa"} />
+      <span className="col-span-2 text-blue-600 font-bold">
+        Wyszukiwanie {<br></br>} Zaawansowane
+      </span>
+      <button className="col-span-2 w-full h-full bg-red-700 text-amber-50 font-bold">
+        Szukaj
+      </button>
+    </div>
+  );
+}
+
+export default MainSerch;
